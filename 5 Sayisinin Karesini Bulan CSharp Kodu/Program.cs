9774681ae4558128a825268262ec95ba850ed08a
yaren <yaren@DESKTOP-9U2BG79>
2024-10-18 01:31:34 +0300

    Add project files.

---
+++ 5 Sayisinin Karesini Bulan CSharp Kodu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_Sayisinin_Karesini_Bulan_CSharp_Kodu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int Sayı = 5;
+
+            int Kare = Sayı * Sayı;
+
+            Console.WriteLine("5 Sayısının Karesi: " + Kare);
+
+            Console.ReadKey();
+
+        }
+    }
+}
